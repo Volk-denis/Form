9bd432b0371431dac7adc57f56527e3dd3ae0ce1
batia-denis <batia-denis@outlook.com>
2018-08-12 23:24:16 +0300

    first chenge

---
+++ ExemplForm/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ExemplForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.but8 = new System.Windows.Forms.Button();
+            this.but1 = new System.Windows.Forms.Button();
+            this.but4 = new System.Windows.Forms.Button();
+            this.but3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.but2 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.but7 = new System.Windows.Forms.Button();
+            this.but10 = new System.Windows.Forms.Button();
+            this.but9 = new System.Windows.Forms.Button();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // but8
+            // 
+            this.but8.Location = new System.Drawing.Point(684, 81);
+            this.but8.Name = "but8";
+            this.but8.Size = new System.Drawing.Size(40, 40);
+            this.but8.TabIndex = 3;
+            this.but8.Text = "Открыть фаил";
+            this.but8.UseVisualStyleBackColor = true;
+            // 
+            // but1
+            // 
+            this.but1.Location = new System.Drawing.Point(275, 40);
+            this.but1.Name = "but1";
+            this.but1.Size = new System.Drawing.Size(40, 40);
+            this.but1.TabIndex = 4;
+            this.but1.Text = "button1";
+            this.but1.UseVisualStyleBackColor = true;
+            // 
+            // but4
+            // 
+            this.but4.Location = new System.Drawing.Point(417, 59);
+            this.but4.Name = "but4";
+            this.but4.Size = new System.Drawing.Size(40, 40);
+            this.but4.TabIndex = 5;
+            this.but4.Text = "button2";
+            this.but4.UseVisualStyleBackColor = true;
+            // 
+            // but3
+            // 
+            this.but3.Location = new System.Drawing.Point(293, 113);
+            this.but3.Name = "but3";
+            this.but3.Size = new System.Drawing.Size(40, 40);
+            this.but3.TabIndex = 6;
+            this.but3.Text = "button3";
+            this.but3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(254, 246);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(40, 40);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // but2
+            // 
+            this.but2.Location = new System.Drawing.Point(800, 221);
+            this.but2.Name = "but2";
+            this.but2.Size = new System.Drawing.Size(40, 40);
+            this.but2.TabIndex = 8;
+            this.but2.Text = "but2";
+            this.but2.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(417, 105);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(40, 40);
+            this.button6.TabIndex = 9;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // but7
+            // 
+            this.but7.Location = new System.Drawing.Point(525, 81);
+            this.but7.Name = "but7";
+            this.but7.Size = new System.Drawing.Size(40, 40);
+            this.but7.TabIndex = 10;
+            this.but7.Text = "button7";
+            this.but7.UseVisualStyleBackColor = true;
+            // 
+            // but10
+            // 
+            this.but10.Location = new System.Drawing.Point(106, 151);
+            this.but10.Name = "but10";
+            this.but10.Size = new System.Drawing.Size(80, 80);
+            this.but10.TabIndex = 11;
+            this.but10.Text = "button8";
+            this.but10.UseVisualStyleBackColor = true;
+            // 
+            // but9
+            // 
+            this.but9.Location = new System.Drawing.Point(589, 81);
+            this.but9.Name = "but9";
+            this.but9.Size = new System.Drawing.Size(40, 40);
+            this.but9.TabIndex = 12;
+            this.but9.Text = "button9";
+            this.but9.UseVisualStyleBackColor = true;
+            // 
+            // groupBox
+            // 
+            this.groupBox.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox.Location = new System.Drawing.Point(396, 151);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(302, 262);
+            this.groupBox.TabIndex = 13;
+            this.groupBox.TabStop = false;
+            this.groupBox.Enter += new System.EventHandler(this.groupBox_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 944);
+            this.Controls.Add(this.but2);
+            this.Controls.Add(this.but10);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.but8);
+            this.Controls.Add(this.but9);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.but1);
+            this.Controls.Add(this.but3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.but7);
+            this.Controls.Add(this.but4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button but8;
+        private System.Windows.Forms.Button but1;
+        private System.Windows.Forms.Button but4;
+        private System.Windows.Forms.Button but3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button but2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button but7;
+        private System.Windows.Forms.Button but10;
+        private System.Windows.Forms.Button but9;
+        private System.Windows.Forms.GroupBox groupBox;
+    }
+}
+
---
+++ ExemplForm/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Text;
+
+namespace ExemplForm
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+            but2.Click += ButClic2;
+            but8.Click += ButClic3;
+            foreach(var iten in this.Controls)
+            {
+                Console.WriteLine(iten);
+                Button b = iten as Button;
+                {
+                    if (b != null)
+                        b.Click += (a, c) =>
+                        {
+                            Button d = (Button)a;
+                            MessageBox.Show(d.Text);
+                        };
+                }
+                    
+            }
+
+            
+
+            
+
+
+
+        }
+
+        private void GroupBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show(String.Format(sender.GetType().ToString()));
+            Button b = null;
+            if(sender is Button)
+                b = (Button)sender;
+
+            
+            if (b != null)
+            {
+                MessageBox.Show($"Была нажата{b.Name}");
+            }
+            
+        }
+
+        private void ButClic3(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                               
+
+                
+                
+            }
+            catch (Exception d)
+            {
+                Console.WriteLine(d.Message);
+            }
+        }
+
+        private void ButClic2(object sender, EventArgs e)
+        {
+            
+
+           
+            
+
+        }
+
+        private void tex_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
